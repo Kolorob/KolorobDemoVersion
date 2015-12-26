32a9f6be0d9f7747ecc6fa83915b2a3c85c467fe
Touhid <tz08128@gmail.com>
2015-12-26 17:28:45 +0600

    Category list width anim. done

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/PlaceDetailsActivity.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/PlaceDetailsActivity.java
@@ -1,21 +1,28 @@
 package demo.kolorob.kolorobdemoversion.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import demo.kolorob.kolorobdemoversion.R;
 import demo.kolorob.kolorobdemoversion.database.CategoryTable;
 import demo.kolorob.kolorobdemoversion.model.CategoryItem;
 import demo.kolorob.kolorobdemoversion.utils.AppConstants;
 import demo.kolorob.kolorobdemoversion.utils.AppUtils;
+import demo.kolorob.kolorobdemoversion.utils.Lg;
 
 /**
  * Created by touhid on 12/3/15.
@@ -25,51 +32,141 @@ import demo.kolorob.kolorobdemoversion.utils.AppUtils;
 public class PlaceDetailsActivity extends BaseActivity {
 
     private static final String TAG = PlaceDetailsActivity.class.getSimpleName();
+    private static final int ANIM_INTERVAL = 100;
 
+    private static double VIEW_WIDTH;
     private ScrollView svCatList;
     private LinearLayout llCatListHolder;
+
+    private boolean isCatExpandedOnce = false;
+    private int primaryIconWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.place_details_activity);
-
-        llCatListHolder = (LinearLayout) findViewById(R.id.llCategoryListHolder);
-        ViewGroup.LayoutParams lp = llCatListHolder.getLayoutParams();
-        lp.width = (int)(AppUtils.getScreenWidth(this) * AppConstants.CAT_LIST_LG_WIDTH_PERC);
+        VIEW_WIDTH = AppUtils.getScreenWidth(this) * AppConstants.CAT_LIST_LG_WIDTH_PERC;
+        isCatExpandedOnce = false;
+        primaryIconWidth = (int) Math.floor(VIEW_WIDTH * 0.80); // 80% of the view width
 
         svCatList = (ScrollView) findViewById(R.id.svCategoryListHolder);
+        llCatListHolder = (LinearLayout) findViewById(R.id.llCategoryListHolder);
+        ViewGroup.LayoutParams lp = llCatListHolder.getLayoutParams();
+        lp.width = (int) (VIEW_WIDTH);
+
         CategoryTable categoryTable = new CategoryTable(PlaceDetailsActivity.this);
         constructCategoryList(categoryTable.getAllCategories());
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // super.onConfigurationChanged(newConfig);
+    }
+
     private void constructCategoryList(ArrayList<CategoryItem> categoryList) {
-        // constructCategoryList(categoryList, 1.0);
+        constructCategoryList(categoryList, 1.0);
+    }
+
+    private void constructCategoryList(ArrayList<CategoryItem> categoryList, double dwPercentage) {
         llCatListHolder.removeAllViews();
 
-        for (CategoryItem ci:categoryList) {
-            // Lg.d(TAG, ci.toString());
-            llCatListHolder.addView(getCategoryListItemView(ci, 1.0));
+        for (CategoryItem ci : categoryList) {
+            llCatListHolder.addView(getCategoryListItemView(ci, dwPercentage));
         }
     }
 
-    private View getCategoryListItemView(CategoryItem ci, double dwPercentage) {
+    private View getCategoryListItemView(final CategoryItem ci, double dwPercentage) {
         LayoutInflater li = LayoutInflater.from(this);
         View v = li.inflate(R.layout.cat_side_list_item, llCatListHolder, false);
         ImageView ivIcon = (ImageView) v.findViewById(R.id.ivIconCatList);
         TextView tvName = (TextView) v.findViewById(R.id.tvNameCatList);
 
-        ivIcon.setImageResource(AppConstants.ALL_CAT_ICONS[ci.getId()-1]);
+        // BE CAREFUL :: Category ID is being mapped as to the icon serial no.
+        // in the AppConstants.ALL_CAT_ICONS array
+        ivIcon.setImageResource(AppConstants.ALL_CAT_ICONS[ci.getId() - 1]);
+        ViewGroup.LayoutParams lpIv = ivIcon.getLayoutParams();
+        lpIv.width = (int) (primaryIconWidth * dwPercentage);
+        ivIcon.setLayoutParams(lpIv);
 
         tvName.setText(ci.getCatName());
+        tvName.setTextSize((float) (VIEW_WIDTH * .10 * dwPercentage));
+
+        // TODO set on-click listener
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> subCatList = getSubCategoryList(ci.getId());
+                Lg.d(TAG, "Sub-ategory populated: " + subCatList.size()
+                        + ", first item=" + subCatList.get(0));
+                if (isCatExpandedOnce)
+                    showAnimatedSubcategories(subCatList, .3); // AppConstants.CAT_LIST_SM_WIDTH_PERC);
+                else
+                    showAnimatedSubcategories(subCatList, 1.0); //AppConstants.CAT_LIST_LG_WIDTH_PERC);
+            }
+        });
 
         return v;
     }
 
-    private void constructCategoryList(ArrayList<CategoryItem> categoryList, double dwPercentage) {
-        /*llCatListHolder.getChi
-        for (CategoryItem ci: categoryList) {
-            llCatListHolder.addView();
-        }*/
+    private ArrayList<String> getSubCategoryList(int id) {
+        // TODO Get sub-categories from the SUB_CATEGORY local table : NEXT PHASE
+
+        ArrayList<String> scList = new ArrayList<>();
+        Collections.addAll(scList, AppConstants.SUB_CATEGORIES[id - 1]);
+        return scList;
+    }
+
+    private void showAnimatedSubcategories(final ArrayList<String> subCatList, double dwPerc) {
+        isCatExpandedOnce = true;
+        decCatListWidth(dwPerc);
+
+        // TODO Inflate the sub-category list from right
+    }
+
+    private void decCatListWidth(final double dwPerc) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Lg.i(TAG, "decCatListWidth : dwPerc = " + dwPerc);
+                if (dwPerc <
+                        (1.0 - AppConstants.CAT_LIST_LG_WIDTH_PERC + AppConstants.CAT_LIST_SM_WIDTH_PERC))
+                    return;
+
+                // Decrease category-list width
+                ViewGroup.LayoutParams lp = llCatListHolder.getLayoutParams();
+                lp.width = (int) (VIEW_WIDTH * dwPerc);
+                llCatListHolder.setLayoutParams(lp);
+
+                int csz = llCatListHolder.getChildCount();
+                for (int i = 0; i < csz; i++) {
+                    View v = llCatListHolder.getChildAt(i);
+                    ImageView iv = (ImageView) v.findViewById(R.id.ivIconCatList);
+                    ViewGroup.LayoutParams lpIv = iv.getLayoutParams();
+                    lpIv.width = (int) (primaryIconWidth * dwPerc);
+                    iv.setLayoutParams(lpIv);
+
+                    /*TextView tv = (TextView) v.findViewById(R.id.tvNameCatList);
+                    tv.setTextSize();*/
+                }
+                decCatListWidth(dwPerc - 0.05);
+            }
+        }, ANIM_INTERVAL);
+    }
+
+    private Animation slideInFromRightAnim() {
+        Animation inFromRight = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, +1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f
+        );
+        inFromRight.setDuration(ANIM_INTERVAL *
+                        (int) (200 *
+                                (AppConstants.CAT_LIST_LG_WIDTH_PERC
+                                        - AppConstants.CAT_LIST_SM_WIDTH_PERC)
+                        )
+        );
+        inFromRight.setInterpolator(new AccelerateInterpolator());
+        return inFromRight;
     }
 }
